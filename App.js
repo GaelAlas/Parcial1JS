6eda5a5101af92a61f7c955aeae3a6490c466e32
GaelAlas <00033625@uca.edu.sv>
2026-09-16 10:36:45 -0600

    feat: implementacion archivo raiz app.js

---
+++ App.js
@@ -0,0 +1,22 @@
+//primero importamos el modulo routes, controllers, utils.
+const express = require('express'); //implementamos el express
+const incidenciasRoutes = require('./routes/incidencias');
+
+//planteamos el puerto en el que se iniciara el servidor
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+
+// Montar las rutas en el prefijo /incidencias
+app.use('/incidencias', incidenciasRoutes);
+
+// Ruta base de prueba
+app.get('/', (req, res) => {
+    res.status(200).send('API REST TechSupport S.A. funcionando correctamente');
+});
+
+//puerto y mensaje cuando el servidor este corriendo correctamente
+app.listen(PORT, () => {
+    console.log(`Servidor corriendo en http://localhost:${PORT}`);
+});
